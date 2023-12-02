aa0d7df4bbf872e94e01b876bae0b81cd70a7ca6
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2023-12-02 14:43:14 +0100

    add Base Button Component

---
+++ src/components/AppButton.vue
@@ -0,0 +1,26 @@
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+    default: "Click me",
+  },
+  iconUrl: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <button
+    class="group flex items-center justify-center gap-2 rounded-full border border-coral-red bg-coral-red px-7 py-4 font-montserrat text-lg leading-none text-white hover:bg-coral-red-500"
+  >
+    {{ props.label
+    }}<img
+      :src="props.iconUrl"
+      alt="Icon"
+      class="h-5 w-5 rounded-full transition duration-200 ease-out group-hover:translate-x-1"
+    />
+  </button>
+</template>
