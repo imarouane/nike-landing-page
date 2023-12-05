19f175fac593a2d84375e819d87a184fbc3e9e10
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2023-12-05 11:26:21 +0100

    fix icon img button issue

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -16,8 +16,9 @@ const props = defineProps({
   <button
     class="group flex items-center justify-center gap-2 rounded-full border border-coral-red bg-coral-red px-7 py-4 font-montserrat text-lg leading-none text-white hover:bg-coral-red-500"
   >
-    {{ props.label
-    }}<img
+    {{ props.label }}
+    <img
+      v-if="iconUrl"
       :src="props.iconUrl"
       alt="Icon"
       class="h-5 w-5 rounded-full transition duration-200 ease-out group-hover:translate-x-1"
